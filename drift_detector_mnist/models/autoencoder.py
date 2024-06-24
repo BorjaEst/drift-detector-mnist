880260612544508ff268b685b65ae4231bc97338
Borja Esteban <boressan@outlook.com>
2024-06-24 08:35:25 +0200

    add autoencoder module

---
+++ drift_detector_mnist/models/autoencoder.py
@@ -0,0 +1,86 @@
+"""Module to define autoencoder models for MNIST dataset."""
+
+import torch
+from torch import nn
+
+
+class Encoder(nn.Module):
+    """Encoder model for the MNIST dataset."""
+
+    def __init__(self, embedding_dim: int):
+        super().__init__()
+
+        self.encoder_conv = nn.Sequential(
+            nn.Conv2d(1, 8, 3, stride=2, padding=1),
+            nn.ReLU(True),
+            nn.Conv2d(8, 16, 3, stride=2, padding=1),
+            nn.BatchNorm2d(16),
+            nn.ReLU(True),
+            nn.Conv2d(16, 32, 3, stride=2, padding=0),
+            nn.ReLU(True),
+        )
+
+        self.flatten = nn.Flatten(start_dim=1)
+
+        self.encoder_lin = nn.Sequential(
+            nn.Linear(3 * 3 * 32, 128),
+            nn.ReLU(True),
+            nn.Linear(128, embedding_dim),
+        )
+
+    def forward(self, x):
+        x = self.encoder_conv(x)
+        x = self.flatten(x)
+        x = self.encoder_lin(x)
+        return x
+
+
+class Decoder(nn.Module):
+    """Decoder model for the MNIST dataset."""
+
+    def __init__(self, embedding_dim: int):
+        super().__init__()
+
+        self.decoder_lin = nn.Sequential(
+            nn.Linear(embedding_dim, 128),
+            nn.ReLU(True),
+            nn.Linear(128, 3 * 3 * 32),
+            nn.ReLU(True),
+        )
+
+        self.unflatten = nn.Unflatten(dim=1, unflattened_size=(32, 3, 3))
+
+        self.decoder_conv = nn.Sequential(
+            nn.ConvTranspose2d(32, 16, 3, stride=2, output_padding=0),
+            nn.BatchNorm2d(16),
+            nn.ReLU(True),
+            nn.ConvTranspose2d(16, 8, 3, stride=2, padding=1, output_padding=1),
+            nn.BatchNorm2d(8),
+            nn.ReLU(True),
+            nn.ConvTranspose2d(8, 1, 3, stride=2, padding=1, output_padding=1),
+        )
+
+    def forward(self, x):
+        x = self.decoder_lin(x)
+        x = self.unflatten(x)
+        x = self.decoder_conv(x)
+        x = torch.sigmoid(x)
+        return x
+
+
+class Autoencoder(nn.Module):
+    """Autoencoder model for the MNIST dataset."""
+
+    def __init__(self, embedding_dim: int) -> None:
+        super().__init__()
+        self.encoder = Encoder(
+            embedding_dim=embedding_dim,
+        )
+        self.decoder = Decoder(
+            embedding_dim=embedding_dim,
+        )
+
+    def forward(self, x):
+        encoded = self.encoder(x)
+        decoded = self.decoder(encoded)
+        return encoded, decoded
